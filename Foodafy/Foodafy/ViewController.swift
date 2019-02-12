d37dac710333c6988cc866ced8ffd2b39799d850
Paul Yi <yipaulc@gmail.com>
2019-02-12 01:45:09 -0800

    Create login screen storyboard

--- Foodafy/Foodafy/ViewController.swift
+++ Foodafy/Foodafy/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var passwordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let attributedString = NSAttributedString(string: "Forgot your Password?", attributes: [NSAttributedString.Key.foregroundColor:UIColor.white, NSAttributedString.Key.underlineStyle:1])
+        
+        passwordButton.setAttributedTitle(attributedString, for: .normal)
+        
     }
-
-
+    
 }
 
