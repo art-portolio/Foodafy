20e4dd0b63f3a1d543f5b8034b62bc012097fd56
Paul Yi <yipaulc@gmail.com>
2019-02-13 08:00:33 -0800

    Create collection view for food photos

---
+++ Foodafy/Foodafy/FoodCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FoodCollectionViewCell.swift
+//  Foodafy
+//
+//  Created by Paul Yi on 2/13/19.
+//  Copyright © 2019 Paul Yi. All rights reserved.
+//
+
+import UIKit
+
+class FoodCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var featuredImageView: UIImageView!
+    @IBOutlet weak var foodTitleLabel: UILabel!
+}
