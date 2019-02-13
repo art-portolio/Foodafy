099f90d8360d8ca13e1ef90c6af0f9d61bdd046c
Paul Yi <yipaulc@gmail.com>
2019-02-13 11:20:53 -0800

    Center scroll view for better user experience

--- Foodafy/Foodafy/FoodViewController.swift
+++ Foodafy/Foodafy/FoodViewController.swift
@@ -29,7 +29,8 @@ class FoodViewController: UIViewController {
         layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
         collectionView?.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: insetY, right: insetX)
         
-        collectionView.dataSource = self
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
     }
     
 }
@@ -51,5 +52,20 @@ extension FoodViewController: UICollectionViewDataSource {
         cell.food = food[indexPath.item]
         
         return cell
+    }
+}
+
+extension FoodViewController: UIScrollViewDelegate, UICollectionViewDelegate {
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let layout = self.collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        
+        var offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        let roundedIndex = round(index)
+        
+        offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
+        targetContentOffset.pointee = offset
     }
 }
