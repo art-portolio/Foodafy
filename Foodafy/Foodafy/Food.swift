622d70fea6257dfc6a9ea65f66315b46759257df
Paul Yi <yipaulc@gmail.com>
2019-02-13 09:40:02 -0800

    Create FoodViewController

--- Foodafy/Foodafy/Food.swift
+++ Foodafy/Foodafy/Food.swift
@@ -21,6 +21,19 @@ class Food {
         self.color = color
     }
     
+    static func fetchFoodPhotos() -> [Food]
+    {
+        return [
+            Food(title: "Grapefruit", featuredImage: UIImage(named: "f1")!, color: UIColor(red: 63/255.0, green: 71/255.0, blue: 80/255.0, alpha: 0.8)),
+            Food(title: "Cake", featuredImage: UIImage(named: "f2")!, color: UIColor(red: 240/255.0, green: 133/255.0, blue: 91/255.0, alpha: 0.8)),
+            Food(title: "Steak", featuredImage: UIImage(named: "f3")!, color: UIColor(red: 105/255.0, green: 80/255.0, blue: 227/255.0, alpha: 0.8)),
+            Food(title: "Pears", featuredImage: UIImage(named: "f4")!, color: UIColor(red: 102/255.0, green: 102/255.0, blue: 102/255.0, alpha: 0.8)),
+            
+            Food(title: "Meatballs", featuredImage: UIImage(named: "f5")!, color: UIColor(red: 245/255.0, green: 62/255.0, blue: 40/255.0, alpha: 0.8)),
+            Food(title: "Eggs", featuredImage: UIImage(named: "f6")!, color: UIColor(red: 103/255.0, green: 217/255.0, blue: 87/255.0, alpha: 0.8)),
+            Food(title: "Pancakes", featuredImage: UIImage(named: "f7")!, color: UIColor(red: 63/255.0, green: 71/255.0, blue: 80/255.0, alpha: 0.8)),
+        ]
+    }
     
 }
 
---
+++ Foodafy/Foodafy/FoodViewController.swift
@@ -0,0 +1,55 @@
+//
+//  FoodViewController.swift
+//  Foodafy
+//
+//  Created by Paul Yi on 2/13/19.
+//  Copyright © 2019 Paul Yi. All rights reserved.
+//
+
+import UIKit
+
+class FoodViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var food = Food.fetchFoodPhotos()
+    let cellScaling: CGFloat = 0.6
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let screenSize = UIScreen.main.bounds.size
+        let cellWidth = floor(screenSize.width * cellScaling)
+        let cellHeight = floor(screenSize.height * cellScaling)
+        
+        let insetX = (view.bounds.width - cellWidth) / 2.0
+        let insetY = (view.bounds.height - cellHeight) / 2.0
+        
+        let layout = collectionView!.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        collectionView?.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: insetY, right: insetX)
+        
+        collectionView.dataSource = self
+    }
+    
+}
+
+extension FoodViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return food.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FoodCell", for: indexPath) as! FoodCollectionViewCell
+        
+        cell.food = food[indexPath.item]
+        
+        return cell
+    }
+}
