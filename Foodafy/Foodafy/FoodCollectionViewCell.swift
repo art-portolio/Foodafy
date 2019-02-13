d74f7a4cab8f0046dfd619e0163f844322a1f8fc
Paul Yi <yipaulc@gmail.com>
2019-02-13 08:46:53 -0800

    Update IBOutlets

--- Foodafy/Foodafy/FoodCollectionViewCell.swift
+++ Foodafy/Foodafy/FoodCollectionViewCell.swift
@@ -12,4 +12,34 @@ class FoodCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var featuredImageView: UIImageView!
     @IBOutlet weak var foodTitleLabel: UILabel!
+    @IBOutlet weak var backgroundColorView: UIView!
+    
+    var food: Food? {
+        didSet {
+            self.updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        if let food = food {
+            featuredImageView.image = food.featuredImage
+            foodTitleLabel.text = food.title
+            backgroundColorView.backgroundColor = food.color
+        } else {
+            featuredImageView.image = nil
+            foodTitleLabel.text = nil
+            backgroundColorView.backgroundColor = nil
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.layer.cornerRadius = 3.0
+        layer.shadowRadius = 2
+        layer.shadowOpacity = 0.8
+        layer.shadowOffset = CGSize(width: 5, height: 10)
+        
+        self.clipsToBounds = false
+    }
 }
