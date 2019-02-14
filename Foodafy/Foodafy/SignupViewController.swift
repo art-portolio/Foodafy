9fdd6005b6f9472eb6572a1d8774b7301daa9313
Paul Yi <yipaulc@gmail.com>
2019-02-14 01:36:59 -0800

    Set up authentication

--- Foodafy/Foodafy/SignupViewController.swift
+++ Foodafy/Foodafy/SignupViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var email: LoginTextField!
+    @IBOutlet weak var password: LoginTextField!
+    @IBOutlet weak var passwordConfirm: LoginTextField!
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        
+        if password.text != passwordConfirm.text {
+            
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
+                
+                if error == nil {
+                    self.performSegue(withIdentifier: "signupToHome", sender: self)
+                } else {
+                    let alertController = UIAlertCOntroller(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
